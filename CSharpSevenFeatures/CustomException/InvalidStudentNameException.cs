054e2c92cb840015c2432980006ecb1e65685020
Brito, Lucas <lucashbrito@gmail.com>
2019-11-26 18:16:36 -0300

    adding custom exception and delegates

---
+++ CSharpSevenFeatures/CustomException/InvalidStudentNameException.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CSharpSevenFeatures.CustomException
+{
+    [Serializable]
+    public class InvalidStudentNameException : Exception
+    {
+        public InvalidStudentNameException()
+        {
+
+        }
+
+        public InvalidStudentNameException(string name) : base($"Invalid student name: {name}")
+        {
+
+        }
+    }
+
+    class Student
+    {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+    }
+
+    public class Example
+    {
+        void Main(string[] args)
+        {
+            Student newStudent = null;
+
+            try
+            {
+                newStudent = new Student();
+                newStudent.StudentName = "James007";
+
+                ValidateStudent(newStudent);
+            }
+            catch (InvalidStudentNameException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadKey();
+        }
+
+        private static void ValidateStudent(Student std)
+        {
+            var regex = new Regex("^[a-zA-Z]+$");
+
+            if (!regex.IsMatch(std.StudentName))
+                throw new InvalidStudentNameException(std.StudentName);
+
+        }
+
+    }
+}
---
+++ CSharpSevenFeatures/Delegate/DelegateClass.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpSevenFeatures.Delegate
+{
+    public class DelegateClass
+    {
+        //Delegate is a function pointer. It is reference type data type.
+        //Syntax: public delegate void <function name>(<parameters>)
+        //A method that is going to assign to delegate must have same signature as delegate.
+        //Delegates can be invoke like a normal function or Invoke() method.
+        //Multiple methods can be assigned to the delegate using "+" operator. It is called multicast delegate.
+
+        public delegate void Print(int value);
+
+        public void Initializer()
+        {
+            PrintHelper(PrintNumber, 10000);
+            PrintHelper(PrintMoney, 10000);
+        }
+
+        public static void PrintHelper(Print delegateFunc, int numToPrint)
+        {
+            delegateFunc(numToPrint);
+        }
+
+        public static void PrintNumber(int num)
+        {
+            Console.WriteLine("Number: {0,-12:N0}", num);
+        }
+
+        public static void PrintMoney(int money)
+        {
+            Console.WriteLine("Money: {0:C}", money);
+        }
+    }
+}
